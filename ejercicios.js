78a3c4f14a6e787210a102726d91885929097dfb
Bautista <bautivargas@gmail.com>
2025-03-11 16:55:36 +0100

    Ejercicios hasta el 11

--- ejercicios.js
+++ ejercicios.js
@@ -1,2 +1,136 @@
 'use strict'
 // EJERCICIO 1
+//  Cuando se haga clic en un botón, cambiará su color de fondo
+// let boton = document.querySelector("button");
+// boton.addEventListener("click",() => {
+//     boton.style.backgroundColor = "violet";
+// })
+
+// ejercicio 2
+// Al hacer clic en el botón, ocultar/mostrar el <div>.
+// let boton = document.querySelector("button");
+// boton.addEventListener("click", () => {
+//     document.querySelector("p").textContent = "Texto cambiado";
+// })
+
+// EJERCICIO 3
+// Al pasar el mouse sobre la imagen, cambiar la src
+// let imagen  = document.querySelector("img");
+// imagen.addEventListener("mouseover",() => {
+//     imagen.src = "imagen2.jpg";
+// })
+
+// EJERCICIO 4
+// Al hacer clic en el botón, ocultar/mostrar el <div>.
+// let botonMostrarOcultar = document.querySelector("button");
+// botonMostrarOcultar.addEventListener("click", () => {
+//     document.querySelector("div").classList.toggle("ocultar");
+// })
+
+// ejericicio 5
+// Contar cuántas veces se hace clic en un botón y mostrarlo.
+// let boton = document.querySelector("button");
+// let contador = 0;
+// boton.addEventListener("click",() => {
+//     contador++;
+//     document.querySelector("p").textContent = "Clics: " + contador;
+// })
+
+// ejercicio 6
+// Al hacer clic en el botón, agregar un nuevo <li> a la lista.
+// let boton = document.querySelector("button");
+// boton.addEventListener("click",()=> {
+//     let nuevoLi = document.createElement("p")
+//     nuevoLi.textContent = "Nuevo li";
+//     document.querySelector("ul").appendChild(nuevoLi);
+// })
+// Ejercicio 7
+// Mostrar en un <p> la tecla que se presionó.
+// let p = document.querySelector("p");
+// document.addEventListener("keydown", (e) => {
+//     p.textContent = e.key;
+// })
+// Ejercicio 8
+// Si se presiona "r", fondo rojo. Si "g", fondo verde. Si "b",fondo azul.
+// let body = document.querySelector("body");
+// document.addEventListener("keydown",(e) => {
+//     if (e.key === "r") {
+//         body.style.backgroundColor = "red";
+//     }else if (e.key === "b") {
+//         body.style.backgroundColor = "blue";
+//     }else if (e.key === "g") {
+//         body.style.backgroundColor = "green";
+//     }
+// })
+// Ejercicio 9
+// Al hacer clic en el botón, cambiar su texto entre "ON" y OFF
+// let boton = document.querySelector("button");
+// boton.addEventListener("click",() =>{
+//     if (boton.textContent === "ON") {
+//         boton.textContent = "OFF";
+//     }else{
+//         boton.textContent = "ON";
+//     }
+// })
+// Ejercicio 10
+// let boton = document.querySelector("button");
+// boton.addEventListener("click",() =>{
+//    let p = document.querySelector("p");
+//     if (p.style.color === "black") {
+//         p.style.color = "red";
+//     }else{
+//         p.style.color = "black";
+//     }
+// })
+// Ejercicio 11
+// Cada <li> tiene un botón "Eliminar".
+// Al hacer clic en el botón, se borra el <li>.
+let botones = document.querySelectorAll("button");
+botones.forEach(nodo => {
+    nodo.addEventListener("click", () => {
+    let padreBoton = nodo.parentElement;
+    let padreLi = padreBoton.parentElement
+    padreLi.removeChild(padreBoton);
+    })
+});
+// Ejercicio 12
+
+// Ejercicio 13
+
+// Ejercicio 14
+
+// Ejercicio 15
+
+// Ejercicio 16
+
+// Ejercicio 17
+
+// Ejercicio 18
+
+// Ejercicio 19
+
+// Ejercicio 20
+
+// Ejercicio 21
+
+// Ejercicio 22
+
+// Ejercicio 23
+
+// Ejercicio 24
+
+// Ejercicio 25
+
+// Ejercicio 26
+
+// Ejercicio 27
+
+// Ejercicio 28
+
+// Ejercicio 29
+
+// Ejercicio 30
+
+// Ejercicio 31
+
+// Ejercicio 32
